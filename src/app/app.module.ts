4a071b366b6b9da1d2c349241fb42cf9fc8c3e16
pberlanda <paolo.berlanda@marconirovereto.it>
2022-04-07 21:03:56 +0200

    ripasso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-@NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
-})
-export class AppModule { }
+import { ContenitoreComponent } from './contenitore/contenitore.component';
+
+export class AppModule {}
---
+++ src/app/contenitore/contenitore.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contenitore',
+  templateUrl: './contenitore.component.html',
+  styleUrls: ['./contenitore.component.css']
+})
+export class ContenitoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
